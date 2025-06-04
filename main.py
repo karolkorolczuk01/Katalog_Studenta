8ce4f1612d1af197c759992fdc73f8d1db386f2c
karolkorolczuk01 <karolkorolczuk@student.wat.edu.pl>
2025-06-04 20:11:24 +0200

    done: wygląd aplikacji okienkowej

--- main.py
+++ main.py
@@ -1,1 +1,109 @@
-print('Siemanko to jest test środowiska')
+from tkinter import *
+
+root = Tk()
+root.geometry("1400x800")
+root.title('StudentBook')
+
+# Konfiguracja siatki (dynamiczne rozciąganie)
+root.columnconfigure(0, weight=1)
+root.columnconfigure(1, weight=1)
+root.columnconfigure(2, weight=1)
+root.rowconfigure(1, weight=1)
+root.rowconfigure(3, weight=1)
+
+# RANKI
+ramka_logowanie = LabelFrame(root, text="🔐 Panel logowania", padx=10, pady=10)
+ramka_uczelnie = LabelFrame(root, text="🏫 Uczelnie", padx=10, pady=10)
+ramka_pracownicy = LabelFrame(root, text="👨‍🏫 Pracownicy", padx=10, pady=10)
+ramka_studenci = LabelFrame(root, text="🎓 Studenci", padx=10, pady=10)
+ramka_mapa = LabelFrame(root, text="🗺️ Mapa", padx=10, pady=10)
+
+ramka_logowanie.grid(row=0, column=0, columnspan=3, sticky="nsew")
+ramka_uczelnie.grid(row=1, column=0, sticky="nsew")
+ramka_pracownicy.grid(row=1, column=1, sticky="nsew")
+ramka_studenci.grid(row=1, column=2, sticky="nsew")
+ramka_mapa.grid(row=3, column=0, columnspan=3, sticky="nsew")
+
+
+
+# LOGOWANIE
+Label(ramka_logowanie, text="Login: ").grid(row=0, column=0)
+entry_login = Entry(ramka_logowanie)
+entry_login.grid(row=0, column=1, sticky="ew", padx=5)
+
+Label(ramka_logowanie, text="Hasło: ").grid(row=0, column=2)
+entry_haslo = Entry(ramka_logowanie, show="*")
+entry_haslo.grid(row=0, column=3, sticky="ew", padx=5)
+
+button_zaloguj = Button(ramka_logowanie, text="Zaloguj")
+button_zaloguj.grid(row=0, column=4, padx=10)
+
+
+
+# UCZELNIE
+listbox_uczelnie = Listbox(ramka_uczelnie, width=70, height=8)
+listbox_uczelnie.grid(row=0, column=0, columnspan=4, sticky="ew")
+
+Label(ramka_uczelnie, text="Województwo:").grid(row=1, column=0, sticky=W)
+entry_wojewodztwo = Entry(ramka_uczelnie)
+entry_wojewodztwo.grid(row=1, column=1, columnspan=3, sticky="ew")
+
+Label(ramka_uczelnie, text="Nazwa uczelni:").grid(row=2, column=0, sticky=W)
+entry_nazwa_uczelni = Entry(ramka_uczelnie)
+entry_nazwa_uczelni.grid(row=2, column=1, columnspan=3, sticky="ew")
+
+button_dodaj_uczelnie = Button(ramka_uczelnie, text="Dodaj").grid(row=4, column=0, sticky="ew")
+button_usun_uczelnie = Button(ramka_uczelnie, text="Usuń").grid(row=4, column=1, sticky="ew")
+button_aktualizuj_uczelnie = Button(ramka_uczelnie, text="Aktualizuj").grid(row=4, column=2, sticky="ew")
+button_mapa_uczelnie = Button(ramka_uczelnie, text="Mapa").grid(row=4, column=3, sticky="ew")
+
+
+
+# PRACOWNICY
+listbox_pracownicy = Listbox(ramka_pracownicy, width=70, height=8)
+listbox_pracownicy.grid(row=0, column=0, columnspan=4, sticky="ew")
+
+Label(ramka_pracownicy, text="Powiat:").grid(row=1, column=0, sticky=W)
+entry_powiat = Entry(ramka_pracownicy)
+entry_powiat.grid(row=1, column=1, columnspan=3, sticky="ew")
+
+Label(ramka_pracownicy, text="Uczelnia:").grid(row=2, column=0, sticky=W)
+entry_uczelnia_dla_pracownika = Entry(ramka_pracownicy)
+entry_uczelnia_dla_pracownika.grid(row=2, column=1, columnspan=3, sticky="ew")
+
+Label(ramka_pracownicy, text="Imię i nazwisko:").grid(row=3, column=0, sticky=W)
+entry_pracownik = Entry(ramka_pracownicy)
+entry_pracownik.grid(row=3, column=1, columnspan=3, sticky="ew")
+
+button_dodaj_pracownicy = Button(ramka_pracownicy, text="Dodaj").grid(row=5, column=0, sticky="ew")
+button_usun_pracownicy = Button(ramka_pracownicy, text="Usuń").grid(row=5, column=1, sticky="ew")
+button_aktualizuj_pracownicy = Button(ramka_pracownicy, text="Aktualizuj").grid(row=5, column=2, sticky="ew")
+button_mapa_pracownicy = Button(ramka_pracownicy, text="Mapa").grid(row=5, column=3, sticky="ew")
+
+
+
+# STUDENCI
+listbox_studenci = Listbox(ramka_studenci, width=70, height=8)
+listbox_studenci.grid(row=0, column=0, columnspan=4, sticky="ew")
+
+Label(ramka_studenci, text="Grupa:").grid(row=1, column=0, sticky=W)
+entry_grupa = Entry(ramka_studenci)
+entry_grupa.grid(row=1, column=1, columnspan=3, sticky="ew")
+
+Label(ramka_studenci, text="Uczelnia:").grid(row=2, column=0, sticky=W)
+entry_uczelnia_studenci = Entry(ramka_studenci)
+entry_uczelnia_studenci.grid(row=2, column=1, columnspan=3, sticky="ew")
+
+Label(ramka_studenci, text="Imię i nazwisko:").grid(row=3, column=0, sticky=W)
+entry_student = Entry(ramka_studenci)
+entry_student.grid(row=3, column=1, columnspan=3, sticky="ew")
+
+Button_dodaj_studenci = Button(ramka_studenci, text="Dodaj").grid(row=4, column=0, sticky="ew")
+Button_usun_studenci = Button(ramka_studenci, text="Usuń").grid(row=4, column=1, sticky="ew")
+Button_aktualizuj_studenci = Button(ramka_studenci, text="Aktualizuj").grid(row=4, column=2, sticky="ew")
+Button_mapa_studenci = Button(ramka_studenci, text="Mapa").grid(row=4, column=3, sticky="ew")
+
+# MAPA
+Label(ramka_mapa, text="Tu mogłaby być mapa lub wizualizacja współrzędnych...").pack(expand=True)
+
+root.mainloop()
